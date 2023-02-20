50fe9824a1647ef4b22b013c730441a65fddeba5
Jeffry Pascual Suero Camilo <jeffrysueroc@gmail.com>
2023-02-19 23:06:56 -0400

    update

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -19,45 +19,59 @@ function Techstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <DiCss3Full/>
+        <h3>Css</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiHtml5/>
+        <h3>HTML</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <h3>JavaScript</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiBootstrap />
+        <h3>Bootstrap</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+        <h3>Node JS</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiTypescript />
+        <h3>TypeScript</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <h3>React</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
+        <h3>MongoDB</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiNextdotjs />
+        <h3>Next</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+        <h3>Git</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
+        <h3>FireBase</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
+        <h3>PostgreSQL</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMysql />
+        <h3>MYSQL</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiCsharp />
+        <h3>C#</h3>
       </Col>
      
     </Row>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -13,18 +13,23 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiLinux />
+        <h3>Linux</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <h3>Visual Studio Code</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <h3>Postman</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiWindows />
+        <h3>Windows</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiHeroku />
+        <h3>Heroku</h3>
       </Col>
     </Row>
   );
